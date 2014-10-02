4a7dcf5ccbec43a05d08ed931e9ccaf02cbd8924
Ruben <ruben.kluge@student.ru.nl>
2014-10-02 19:50:59 +0200

    test add

--- Colorclicker/src/Main.java
+++ Colorclicker/src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.SwingUtilities;
 
 
 public class Main {
-
+	// testing
 	public static void main(String... args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
