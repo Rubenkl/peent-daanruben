962ca62b3a6bc3cf878c28f5d72aad10d1e93081
Ruben <ruben.kluge@student.ru.nl>
2014-10-07 22:27:40 +0200

    upload audio naar google speech. Werkt nog niet helemaal

--- Colorclicker/src/Speaking.java
+++ Colorclicker/src/Speaking.java
@@ -1,48 +1,52 @@
-/*
-// This is a basic example to demonstrate how the Speech-To-Text Library 
-// can be used. See http://stt.getflourish.com for more information on
-// available settings.
-//
-// Florian Schulz 2014, www.getflourish.com
-*/
-
-import com.getflourish.stt.*;
-
-STT stt;
-String results;
+//package recognitionprocess;
+//import org.jaudiotagger.audio.*;
 
 
-void setup ()
-{
-  size(600, 200);
-  // Init STT with default manual record mode
-  String key = "AIzaSyAw7MbFTrcMZlR6Rx3gsMUMHjnAuxdgFto";
-  stt = new STT(this, key);
-  stt.enableDebug();
-  stt.setLanguage("en"); 
-  
-  // Some text to display the result
-  textFont(createFont("Arial", 24));
-  result = "Say something!";
-}
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-void draw ()
-{
-  background(0);
-  text(result, mouseX, mouseY);
-}
+import javax.sound.sampled.AudioFileFormat;
 
-// Method is called if transcription was successfull 
-void transcribe (String utterance, float confidence) 
-{
-  println(utterance);
-  result = utterance;
-}
+import com.darkprograms.speech.microphone.Microphone;
+import com.darkprograms.speech.recognizer.GSpeechDuplex;
+import com.darkprograms.speech.recognizer.GSpeechResponseListener;
+import com.darkprograms.speech.recognizer.GoogleResponse;
 
-// Use any key to begin and end a record
-public void keyPressed () {
-  stt.begin();
-}
-public void keyReleased () {
-  stt.end();
+public class Speaking {
+
+	public static void main(String[] args){
+	    GSpeechDuplex dup = new GSpeechDuplex("AIzaSyAw7MbFTrcMZlR6Rx3gsMUMHjnAuxdgFto");//Instantiate the API
+	    dup.addResponseListener(new GSpeechResponseListener(){// Adds the listener
+	        public void onResponse(GoogleResponse gr){
+	            System.out.println("Google thinks you said: " + gr.getResponse());
+	            System.out.println("with " + 
+	            ((gr.getConfidence()!=null)?(Double.parseDouble(gr.getConfidence())*100):null) 
+	                + "% confidence.");
+	            System.out.println("Google also thinks that you might have said:" 
+	                    + gr.getOtherPossibleResponses());
+	        }
+	    });
+	    Microphone mic = new Microphone(AudioFileFormat.Type.WAVE);//Instantiate microphone and have 
+	    // it record FLAC file.
+	    File file = new File("CRAudioTest.wav");//The File to record the buffer to. 
+	    //You can also create your own buffer using the getTargetDataLine() method.
+	    while(true){
+	        try{
+	            mic.captureAudioToFile(file);//Begins recording
+	            Thread.sleep(5000);//Records for 10 seconds
+	            mic.close();//Stops recording
+	            //Sends 10 second voice recording to Google
+	            Path path = Paths.get("C:/Users/Ruben/git/peent-daanruben/Colorclicker/CRAudiotest.wav");
+	            byte[] data = Files.readAllBytes(path);//Saves data into memory.
+	                    dup.recognize(data, (int)mic.getAudioFormat().getSampleRate());
+	            mic.getAudioFile().delete();//Deletes Buffer file
+	            //REPEAT
+	        }
+	        catch(Exception ex){
+	            ex.printStackTrace();//Prints an error if something goes wrong.
+	        }
+	    }
+	}
 }
